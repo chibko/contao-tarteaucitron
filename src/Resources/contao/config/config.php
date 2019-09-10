cf73e7662c1a87cd4cfdddffd3f9430de5396133
chibko <info@chibko.com>
2019-09-10 08:50:58 +0200

    Fix bugs

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 $GLOBALS['TL_HOOKS']['generatePage'][] = array('Chibko\Contao\Tarteaucitron\CookieCitron', 'addCookieCitronScripts');
-$GLOBALS['TL_HOOKS']['getCacheKey'][] = array('Chibko\Contao\Tarteaucitron\CCookieCitron', 'modifyCacheKey');
+$GLOBALS['TL_HOOKS']['getCacheKey'][] = array('Chibko\Contao\Tarteaucitron\CookieCitron', 'modifyCacheKey');
 $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = array('Chibko\Contao\Tarteaucitron\CCookieCitron', 'addCookieCitronBuffer');
