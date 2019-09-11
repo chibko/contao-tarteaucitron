1985b2d4b8bdbe6676555e1342035fc53f2c2059
chibko <info@chibko.com>
2019-09-11 09:09:52 +0200

    Delete files and update lnaguages files

--- src/Resources/contao/languages/en/tl_page.php
+++ src/Resources/contao/languages/en/tl_page.php
@@ -1,57 +1,35 @@
 <?php
 
 /**
- * cookiebar extension for Contao Open Source CMS
- *
- * Copyright (C) 2013 Codefog
- *
- * @package cookiebar
- * @author  Codefog <http://codefog.pl>
- * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
- * @license LGPL
- */
-
-
-/**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_enable']        = [
-    'Enable cookie bar',
-    'Display the cookie information bar on the website.',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_message']       = [
-    'Cookies information',
-    'Please enter a short information about the cookies.',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_jumpTo']        = [
-    'Details page',
-    'Here you can choose the page with more information about the cookies.',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']      = ['Bar position', 'Here you can choose the bar position.'];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']     = [
-    'Bar placement in DOM',
-    'Here you can choose the bar placement in DOM structure.',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_button']        = [
-    'Button label',
-    'Please enter the button label (e.g. <em>Accept</em>).',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_combineAssets'] = [
-    'Combine assets',
-    'Adds the cookiebar CSS and JS assets to the combined file.',
-];
-
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_enable'] = ['Enable cookie bar','Display the cookie information bar on the website.'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_highPrivacy'] = ['Disable implied consent','Disable implied consent (while browsing)'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_url_privacy'] = ['Privacy page','Choose the page describing the privacy policy'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_AcceptAllCta'] = ['Button "Accept all"','Check this box to display the "Accept All" button'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_adblocker'] = ['Ad blocker','Check this box to display a message if an adblocker is detected'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_cookieslist'] = ['List of installed cookies','Check this box to display the list of installed cookies'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_removeCredit'] = ['Remove credits','Check this box to remove the link to the tarteaucitron.js website'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_showAlertSmall'] = ['Bottom banner','Check this box to display the small banner at the bottom right'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position'] = ['Bar position', 'Here you can choose the bar position.'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement'] = ['Bar placement in DOM','Here you can choose the bar placement in DOM structure.'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_combineAssets'] = ['Combine assets','Adds the cookiebar CSS and JS assets to the combined file.'];
 
 /**
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_page']['cookiecitron_legend'] = 'Cookie information';
 
-
 /**
  * Reference
  */
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['top']             = 'Top of the page';
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['bottom']          = 'Bottom of the page';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['top'] = 'Top of the page';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['bottom'] = 'Bottom of the page';
 $GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['before_wrapper'] = 'Before the #wrapper';
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['body_end']       = 'Before &lt;body&gt; end';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['body_end'] = 'Before &lt;body&gt; end';
+
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['ga'] = 'Google Analytics (ga.js)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtag'] = 'Google Analytics (gtag.js)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtag_multiple_ua'] = 'Google Analytics (gtag.js) for multiple UA';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['ga_universal'] = 'Google Analytics (universal)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtm'] = 'Google Tag Manager';
--- src/Resources/contao/languages/fr/tl_page.php
+++ src/Resources/contao/languages/fr/tl_page.php
@@ -1,67 +1,35 @@
 <?php
 
 /**
- * cookiebar extension for Contao Open Source CMS
- *
- * Copyright (C) 2013 Codefog
- *
- * @package cookiebar
- * @author  Codefog <http://codefog.pl>
- * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
- * @license LGPL
- */
-
-
-/**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_enable']        = [
-    'Activer le script Tarte au Citron',
-    'Affiche les informations cookies sur le site',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_highPrivacy']        = [
-    'Désactiver le consentement implicite',
-    'Désactiver le consentement implicite (en naviguant)',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_adblocker']        = [
-    'Ad blocker',
-    'Cochez cette case pour afficher un message si un adblocker est détecté',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_cookieslist']        = [
-    'Liste des cookies installés',
-    'Cochez cette case pour afficher la liste des cookies installés',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_removeCredit']        = [
-    'Supprimer crédits',
-    'Cochez cette case pour supprimer le lien vers le site tarte au citron',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_showAlertSmall']        = [
-    'Bandeau bas',
-    'Cochez cette case pour afficher le petit bandeau en bas à droite',
-];
-
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']      = ['Position de la barre', 'Choisissez où afficher la barre : Haut ou bas'];
-
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']     = [
-    'Placement du code dans le DOM',
-    'Choisissez le placement du code dans la structure du DOM.',
-];
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_combineAssets'] = [
-    'Combiner les CSS',
-    'Combine et compresse la feuille de style tarteaucitron.css avec les autres CSS.',
-];
-
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_enable'] = ['Activer le script Tarte au Citron','Affiche les informations cookies sur le site'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_highPrivacy'] = ['Désactiver le consentement implicite','Désactiver le consentement implicite (en naviguant)'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_url_privacy'] = ['Page Confidentialité','Choisir la page décrivant la politique de confidentialité'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_AcceptAllCta'] = ['Bouton "Tout accepter"','Cochez cette case pour afficher le bouton "Tout accepter"'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_adblocker'] = ['Ad blocker','Cochez cette case pour afficher un message si un adblocker est détecté'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_cookieslist'] = ['Liste des cookies installés','Cochez cette case pour afficher la liste des cookies installés'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_removeCredit'] = ['Supprimer crédits','Cochez cette case pour supprimer le lien vers le site tarte au citron'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_showAlertSmall'] = ['Bandeau bas','Cochez cette case pour afficher le petit bandeau en bas à droite'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position'] = ['Position de la barre', 'Choisissez où afficher la barre : Haut ou bas'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement'] = ['Placement du code dans le DOM','Choisissez le placement du code dans la structure du DOM.'];
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_combineAssets'] = ['Combiner les CSS','Combine et compresse la feuille de style tarteaucitron.css avec les autres CSS.'];
 
 /**
  * Legends
  */
 $GLOBALS['TL_LANG']['tl_page']['cookiecitron_legend'] = 'Informations sur les cookies';
 
-
 /**
  * Reference
  */
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['top']             = 'Haut de page';
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['bottom']          = 'Bas de page';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['top'] = 'Haut de page';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_position']['bottom'] = 'Bas de page';
 $GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['before_wrapper'] = 'Avant la balise #wrapper';
-$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['body_end']       = 'Avant la balise de fin &lt;body&gt;';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_placement']['body_end'] = 'Avant la balise de fin &lt;body&gt;';
+
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['ga'] = 'Google Analytics (ga.js)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtag'] = 'Google Analytics (gtag.js)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtag_multiple_ua'] = 'Google Analytics (gtag.js) for multiple UA';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['ga_universal'] = 'Google Analytics (universal)';
+$GLOBALS['TL_LANG']['tl_page']['cookiecitron_services']['gtm'] = 'Google Tag Manager';
