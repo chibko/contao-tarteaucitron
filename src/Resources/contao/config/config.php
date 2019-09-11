20edac6b7fff2557e78408659b9cfd3cd04e4bc0
chibko <info@chibko.com>
2019-09-11 09:11:05 +0200

    Remove the getCacheKey hook

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,4 @@
 <?php
 
 $GLOBALS['TL_HOOKS']['generatePage'][] = array('Chibko\Contao\Tarteaucitron\CookieCitron', 'addCookieCitronScripts');
-$GLOBALS['TL_HOOKS']['getCacheKey'][] = array('Chibko\Contao\Tarteaucitron\CookieCitron', 'modifyCacheKey');
 $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = array('Chibko\Contao\Tarteaucitron\CookieCitron', 'addCookieCitronBuffer');
