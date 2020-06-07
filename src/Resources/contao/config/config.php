5d83380d7a38c56f16f5c9b529799e23b4809040
chibko <info@chibko.com>
2020-06-07 10:39:46 +0200

    Remove config.php

--- src/Resources/contao/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_HOOKS']['generatePage'][] = array('Chibko\Tarteaucitron\CookieCitron', 'addCookieCitronScripts');
-$GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = array('Chibko\Tarteaucitron\CookieCitron', 'addCookieCitronBuffer');
